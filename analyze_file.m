ef46ef09dd69e95f8e48d5a004f5ca13b869d03f
Shirley <80057848+hohilwik@users.noreply.github.com>
2021-08-07 02:57:49 +0530

    read/analyze function for q1

---
+++ analyze_file.m
@@ -0,0 +1,21 @@
+function [message, symbols, probability] = analyze_file(filename, type)
+fileId=fopen(filename,'r');
+message=fscanf(fileId,'%c');
+fclose(fileId);
+if type==0
+symbols=[];
+probability=[];
+n=length(message);
+i=1;
+while ~isempty(message)
+    [mode_char,no_occurrances]=mode(message); %find the most occuring character and its number of occurences
+    symbols(i)=mode_char;                     %store that character in symbols vector
+    probability(i)=no_occurrances;            %store its #occurence in probability vector
+    indices=find(message==mode_char);         %remove all instances of the character from message
+    message(indices)='';
+    i=i+1;
+end
+probability=probability/n;
+end
+end
+
